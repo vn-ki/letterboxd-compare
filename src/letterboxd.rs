cfaf77a8f464e5a93ba1a61b480cd3720a685808
Steve Hammond <steve@fullofstars.space>
2025-02-02 14:12:51 -0700

    parse the span the hard way

--- src/letterboxd.rs
+++ src/letterboxd.rs
@@ -98,7 +98,7 @@ impl LetterboxdClient {
         }
     }
 
-    fn parse_rating2(rating: String) -> integer {
+    fn parse_rating2(rating: String) -> u8 {
         if rating.contains("rated-1") {return Ok(1 as u8);}
         if rating.contains("rated-2") {return Ok(2 as u8);}
         if rating.contains("rated-3") {return Ok(3 as u8);}
